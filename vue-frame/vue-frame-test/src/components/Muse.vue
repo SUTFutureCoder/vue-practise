04b7e30bdfe5c979aef74a13ee8aac34965c0776
linxingchen <linxingchen@iwaimai.baidu.com>
2017-02-08 20:56:17 +0800

    my first vue page

---
+++ vue-frame/vue-frame-test/src/components/Muse.vue
@@ -0,0 +1,35 @@
+<template>
+  <mu-card>
+    <mu-card-media>
+      <mu-date-picker hintText="竖屏模式选择"/> <br/>
+      <mu-date-picker mode="landscape" hintText="横屏模式选择"/> <br/>
+      <mu-time-picker hintText="12小时制"/><br/>
+      <mu-time-picker hintText="24小时制" format="24hr"/><br/>
+      <mu-time-picker hintText="横屏显示" mode="landscape"/><br/>
+      <mu-date-picker hintText="不可用" disabled/> <br/>
+    </mu-card-media>
+    <mu-card-actions>
+      <mu-raised-button label="屠龙宝刀，点击就送" primary/>
+    </mu-card-actions>
+  </mu-card>
+</template>
+
+<script>
+export default {
+  name: 'muse',
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="css">
+  .mu-card{
+    width: 50%;
+    margin: 0 auto;
+    margin-bottom: 2px;
+  }
+</style>
